909808329966f1d3ce77e8fe1bf0c21bd4af5b84
黃敏雄 <64833384+MinXiong-Huang@users.noreply.github.com>
2021-01-31 12:04:54 +0800

    Update Download_Music_from_YouTube.py

--- Download_Music_from_YouTube.py
+++ Download_Music_from_YouTube.py
@@ -12,17 +12,17 @@ def progress(chunk, file_handle, bytes_remaining):
     ('█' * int(size*20/contentSize), ' '*(20-int(size*20/contentSize)), float(size/contentSize*100)), end='')
 
 
-Youtube_url =input('請輸入網址:')
+Youtube_url = input('請輸入網址:')
 yt = YouTube(Youtube_url, on_progress_callback=progress)
-video =yt.streams.filter(only_audio=True).first()
+video = yt.streams.filter(only_audio=True).first()
 video.download()
 print('下載完畢!\n')
 
 print('請選擇剛才下載完的MP4檔!')
 Video_url = easygui.fileopenbox() #選擇Video檔案路徑
-FileName=Video_url.split('\\') #字串切割
-Music_Name=FileName[-1] #音樂檔名
-Music_Name=Music_Name.split('.') #字串切割
+FileName = Video_url.split('\\') #字串切割
+Music_Name = FileName[-1] #音樂檔名
+Music_Name = Music_Name.split('.') #字串切割
 
 #更改檔名
 Old_name = str(Music_Name[0])+'.mp4'
